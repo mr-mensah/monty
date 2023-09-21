f493bf4ad250a3b4b72886580e38b45ee83c4a22
mr-mensah <108750327+mr-mensah@users.noreply.github.com>
2023-09-21 13:24:47 +0000

    Update fresh.c

--- fresh.c
+++ fresh.c
@@ -10,7 +10,7 @@ void a_pop(stack_t **head, unsigned int counter)
 stack_t *h;
 if (*head == NULL)
 {
-vprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 fclose(bus.file);
 free(bus.content);
 emp_stacks(*head);
